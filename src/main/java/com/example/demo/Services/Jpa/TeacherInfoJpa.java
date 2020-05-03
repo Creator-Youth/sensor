2f94c2baf9cbe27021bacd01d5c77053ebd64a69
wangxianshou <wangxianshou@hyperchain.cn>
2020-05-03 21:57:23 +0800

    5-3,bug fix -5

--- src/main/java/com/example/demo/Services/Jpa/TeacherInfoJpa.java
+++ src/main/java/com/example/demo/Services/Jpa/TeacherInfoJpa.java
@@ -6,6 +6,13 @@ package com.example.demo.Services.Jpa;/*
 
 import com.example.demo.Dao.Teacher_Info;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TeacherInfoJpa extends JpaRepository<Teacher_Info,Integer> {
+    @Query("select tea from Teacher_Info tea where teacher_id = ?1")
+    Teacher_Info getByTeacherId(String teacher_id);
+
+    @Query("select tea from Teacher_Info tea where teacher_name = ?1")
+    Teacher_Info getByTeacherName(String teacher_name);
+
 }
