333866521910323109aedfc8ec43dcc830790a70
huajishaonian <285528884@qq.com>
2020-05-09 10:51:51 +0800

    测试图标显示问题

---
+++ src/main/java/com/example/demo/Services/DefaultView.java
@@ -0,0 +1,23 @@
+package com.example.demo.Services;/*
+ *  @author huajishaonian
+ *  time : 2020-05-2020/5/8-2:37 下午
+ *
+ */
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * @author chenjun
+ */
+//@Configuration
+/*public class DefaultView implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("hello");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}*/
